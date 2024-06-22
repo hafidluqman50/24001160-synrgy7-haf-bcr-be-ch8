ed73e793c5b1092f1d35dbfef98670a5b028bc79
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2024-06-22 20:18:08 +0800

    test(cars): update test cars available using jest supertest

--- index.ts
+++ index.ts
@@ -16,3 +16,28 @@ moduleAlias.addAliases({
 })
 
 import '@routes/app'
+
+console.log(`
+  |       API ENDPOINT      | METHOD |        DESCRIPTION        |
+  |-------------------------|--------|---------------------------|
+  |--------------------------- CARS -----------------------------|
+  | /api/cars               |   GET  | Get All Data Cars         |
+  | /api/cars/:id           |   GET  | Get By Id Data Cars       |
+  | /api/cars/              |  POST  | Post Data Cars            |
+  | /api/cars/:id           |   PUT  | Update Data Cars By Id    |
+  | /api/cars/:id           | DELETE | Delete Data Cars By Id    |
+  | /api/cars/list-available|   GET  | Get List Available Cars   |
+  | /api/cars/log-activity  |   GET  | Get Log Activity Cars     |
+  |--------------------------- AUTH -----------------------------|
+  | /api/super-admin/login  |  POST  | Login Super Admin         |
+  | /api/admin/login        |  POST  | Login Admin               |
+  | /api/member/register    |  POST  | Register Member           |
+  | /api/member/login       |  POST  | Login Post Login          |
+  | /api/current-user       |   GET  | Get Current User By Token |
+  |--------------------------- USERS ----------------------------|
+  | /api/user-admin         |  GET   | Get All User Admin        |
+  | /api/user-admin         | POST   | Create User Admin         |
+  | /api/user-admin/:id     | GET    | Get User Admin By Id      |
+  | /api/user-admin/:id     | PUT    | Update User Admin By Id   |
+  | /api/user-admin/:id     | DELETE | Delete User Admin By Id   |
+`)
